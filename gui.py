88a1c9931d900a05b9a7e23c4b27e88fe7e574ac
Ryusei Tomikawa <77111746+Ryusei-Tomikawa@users.noreply.github.com>
2022-10-29 14:59:27 +0900

    Add files via upload

--- gui.py
+++ gui.py
@@ -29,7 +29,7 @@ def get_ymd():
 
 def write_label(txt, f, f_num, x_, y_):
     
-    title_label = ttk.Label(text=txt, font=(f, f_num, "bold"))
+    title_label = ttk.Label(text=txt, justify="center", font=(f, f_num, "bold"))
     #左上が(0,0)
     title_label.place(x=x_, y=y_)
 
@@ -41,11 +41,11 @@ def karihozon_check():
 
     yes_karihozon = BooleanVar(value = False)
     chk_yes_karihozon = ttk.Checkbutton(variable=yes_karihozon, text='仮保存する', command=lambda:check_click(yes_karihozon.get(), '仮保存する')) 
-    chk_yes_karihozon.place(x=70, y=235)
+    chk_yes_karihozon.place(x=70, y=285)
 
     no_karihozon = BooleanVar(value = False)
     chk_no_karihozon = ttk.Checkbutton(variable=no_karihozon, text='仮保存しない', command=lambda:check_click(no_karihozon.get(), '仮保存しない')) 
-    chk_no_karihozon.place(x=70, y=280)
+    chk_no_karihozon.place(x=70, y=330)
 
 def error(message):
         messagebox.showerror('エラーメッセージ', message)
@@ -99,14 +99,12 @@ def get_combobox(text_, num):
     # ex. 2025(out) 10(ok) の場合、2025がoutだから10が性格でもcountが＋１にならない
     # ここのエラー処理は拘るべきだと思うけど。。。
 
-    if date_now.year <  from_year or date_now.year < to_year:
-        error('その年数はまだ入力できません')
-        count -= 1
-    elif date_now.month < from_month or date_now.month < to_month:
+    if date_now.month < from_month or date_now.month < to_month:
         error('その月はまだ入力できません')
         count -= 1
     elif (date_now.month == from_month or date_now.month == to_month) and (date_now.day < from_day or date_now.day < to_day):
         error('その日はまだ入力できません')
+        count -= 1
 
     
 # 日付入力する関数
@@ -143,10 +141,8 @@ def entry(txt_, list_, width_, x_, y_):
 
 def check_click(chk_state, chk_txt):
         
-        
         global yes_karihozon
         global no_karihozon
-
 
         # get_ymdk関数用のグルーバル変数
         global isKarihozon_
@@ -227,18 +223,44 @@ def create_gui():
     #メインウィンドウ立ち上げ
     root = Tk()
     root.title('Tomikawa Create GUI')
-    root.geometry('500x360')
+    root.geometry('500x400')
 
     # ウィジェットの作成 padding=フレームの内側とWidgetとの間に空の領域を作成する
     frame = ttk.Frame(root, padding=10)
     frame.pack()
 
+    # 現在日時取得
+    date_now = datetime.datetime.now()
+    today = datetime.datetime.today()
+
+    if today.weekday() == 0:
+        day_txt = '月'
+    elif today.weekday() == 1:
+        day_txt = '火'
+    elif today.weekday() == 2:
+        day_txt = '水'
+    elif today.weekday() == 3:
+        day_txt = '木'
+    elif today.weekday() == 4:
+        day_txt = '金'
+    elif today.weekday() == 5:
+        day_txt = '土'
+    elif today.weekday() == 6:
+        day_txt = '日'
+
     # タイトル
-    write_label('安川電機  交通費申請自動化GUI', "MSゴシック", "20", 40, 50)
+    write_label('安川電機  交通費申請自動化GUI', "MSゴシック", "20", 40, 55)
     #作成者
-    write_label('作成者: 冨川 竜誠', "Times", "10", 370, 95)
+    write_label('作成者: 冨川 竜誠', "Times", "10", 370, 100)
+    # 今日の日付
+    write_label('今日の日付は', "MSゴシック", "12", 80, 130)
+    write_label(str(date_now.year) + '年', "MSゴシック", "12", 190, 130)
+    write_label(str(date_now.month) + '月', "MSゴシック", "12", 245, 130)
+    write_label(str(date_now.day) + '日', "MSゴシック", "12", 285, 130)
+    write_label(day_txt  + '曜日です', "MSゴシック", "12", 325, 130)
+
     #申請する日付
-    write_label('申請する日付:', "Times", "12", 20, 130)
+    write_label('申請する日付:', "Times", "12", 35, 180)
 
     # 安川画像表示
     yaskawa = tk.PhotoImage(file='yaskawa.png')
@@ -252,46 +274,43 @@ def create_gui():
     count = 0
 
     # 現在の年数から5年前まで
-    # 現在日時取得
-    date_now = datetime.datetime.now()
-
     year = []
     for i in range(6):
         year.append((date_now.year - 5) + i)
     # 年数入力 ～から
-    entry('from_year', year, 4, 140, 130)
+    entry('from_year', year, 4, 155, 180)
     # ～まで
-    entry('to_year', year, 4, 140, 180)
+    entry('to_year', year, 4, 155, 230)
     # 年ラベル ～から
-    write_label('年', "Times", "12", 195, 130)
+    write_label('年', "Times", "12", 210, 180)
     # ～まで
-    write_label('年', "Times", "12", 195, 180)
+    write_label('年', "Times", "12", 210, 230)
 
     # 1～12月
     month = []
     for i in range(12):
         month.append(i+1)
     # 月入力 ～から
-    entry('from_month', month, 4, 220, 130)
+    entry('from_month', month, 4, 235, 180)
     # ～まで
-    entry('to_month', month, 4, 220, 180)
+    entry('to_month', month, 4, 235, 230)
     # 月ラベル  ～から
-    write_label('月', "Times", "12", 270, 130)
+    write_label('月', "Times", "12", 285, 180)
     # ～まで
-    write_label('月', "Times", "12", 270, 180)
+    write_label('月', "Times", "12", 285, 230)
 
     # 1～31日
     day = []
     for i in range(31):
         day.append(i+1)
     # 日入力 ～から
-    entry('from_day', day, 4, 295, 130)
+    entry('from_day', day, 4, 310, 180)
     # ～まで
-    entry('to_day', day, 4, 295, 180)
+    entry('to_day', day, 4, 310, 230)
     # 日ラベル ～から
-    write_label('日  から', "Times", "12", 345, 130)
+    write_label('日  から', "Times", "12", 360, 180)
     # ～まで
-    write_label('日  まで', "Times", "12", 345, 180)
+    write_label('日  まで', "Times", "12", 360, 230)
 
     ###########################################   仮保存する or しない  ###########################################
 
@@ -301,9 +320,9 @@ def create_gui():
     ###########################################       ボタン処理        ############################################
 
     # 開始ボタン
-    push_button("開始", "交通費自動化申請を開始しますか?", 20, 300, 230)
+    push_button("開始", "交通費自動化申請を開始しますか?", 20, 300, 280)
     #ウィンドウ閉じるボタン
-    push_button("終了", "ウィンドウを閉じてもよろしいですか？", 20, 300, 280)
+    push_button("終了", "ウィンドウを閉じてもよろしいですか？", 20, 300, 330)
 
     # ウィンドウの表示するための無限ループ
     root.mainloop()
